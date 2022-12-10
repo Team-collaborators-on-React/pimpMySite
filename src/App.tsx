0fe604b58afcb55cf979f8e09e8383e62d980ec0
Vladon305 <shuhow003@mail.ru>
2022-12-10 22:24:34 +0300

    removed dir nav and remake trantsition

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './components/home_page/HomePage'
 import './App.scss'
 
 const App = () => {
-    return <HomePage />
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+    )
 }
 
 export default App
